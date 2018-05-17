b212bcb4466e4c45508e436ec23623a02cf5d892
limjongsu <limjongsu@imjongsuui-MacBook-Pro.local>
2018-05-17 16:45:22 +0900

    change namespae and nodeSelector, pv

---
+++ config/kibana/create_kb.sh
@@ -0,0 +1,5 @@
+kubectl create -f kibana-config.yaml
+kubectl create -f kibana-deployment.yaml
+kubectl create -f kibana-ingress.yaml
+kubectl create -f kibana-nginx.yaml
+kubectl create -f kibana-service.yaml 
---
+++ config/kibana/delete_kb.sh
@@ -0,0 +1,5 @@
+kubectl delete -f kibana-config.yaml
+kubectl delete -f kibana-deployment.yaml
+kubectl delete -f kibana-ingress.yaml
+kubectl delete -f kibana-nginx.yaml
+kubectl delete -f kibana-service.yaml 
