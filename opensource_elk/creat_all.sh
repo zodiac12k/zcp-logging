d923608062ad1795bfe6cac434c166be95aad15e
limjongsu <limjongsu@imjongsuui-MacBook-Pro.local>
2018-05-28 15:26:20 +0900

    namespace chage : logging -> zcp-logging

--- opensource_elk/creat_all.sh
+++ opensource_elk/creat_all.sh
@@ -1,8 +1,8 @@
-cd /Users/limjongsu/git/zcp-logging/icp_elk/filebeat/
+cd /Users/limjongsu/git/zcp-logging/opensource_elk/filebeat/
 ./create_fb.sh
-cd /Users/limjongsu/git/zcp-logging/icp_elk/elasticsearch/
+cd /Users/limjongsu/git/zcp-logging/opensource_elk/elasticsearch/
 ./create_es.sh
-cd /Users/limjongsu/git/zcp-logging/icp_elk/kibana/
+cd /Users/limjongsu/git/zcp-logging/opensource_elk/kibana/
 ./create_kb.sh
-cd /Users/limjongsu/git/zcp-logging/icp_elk/logstash/
+cd /Users/limjongsu/git/zcp-logging/opensource_elk/logstash/
 ./create_ls.sh
